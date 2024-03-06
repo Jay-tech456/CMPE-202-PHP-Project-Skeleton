f63bd03c5d4e4f256998bdf466df67620bf00b3a
Manjesh Prasad <manjeshprasad@Manjeshs-Air.hsd1.ca.comcast.net>
2024-03-06 15:59:39 -0800

    updated everything

--- public/index.php
+++ public/index.php
@@ -7,37 +7,64 @@
   </head>
 
   <body>
-    <section class = "Cover" >
+    <section class = "Cover container" >
 
       <div class = "front-text">
-      <h1> Welcome to Coffee Espresso </h1>
+      <h1> Coffee Ex-Press-O </h1>
     </div>
       </section>
 
-    <section id = "about">
+    <section id = "about" class = "container">
 
       <h1> About Us </h1>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <h4>
+
+        Founded in 2024, at the height of the massive layoffs, we are a small coffee delivery service that scours the entire internet to find the highest
+         quality coffee for our customers. As remote work becomes more prevalent, we've noticed a significant demand for caffeine among our customers, many 
+         of whom are reluctant to go through the hassle of ordering coffee through an app. With our service, we deliver coffee straight to your door, requiring 
+         no effort on your part. The service is simple: pay a monthly subscription, and our suppliers will deliver your coffee in a mystery container every 2 weeks. As the 
+         consumer, you won't have to do anything except wait for it to arrive. Subscriptions are billed every three months.
+      </h4>
     </section>
 
 
     <section id = "services">
 
       <h1> Services </h1>
-        <div class = card-container" id="cardContainer"> </div> 
+        <div id="cardContainer"> </div> 
     </section>
+    
 
     <section id = "News">
 
       <h1> News </h1>
-
+      <div id="newsCardContainer"> </div> 
     </section>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+
     <section id = "contactUs">
       <h1> Contact Us </h1>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <?php 
+        // find the file path 
+        $filePath = "contactInfo.txt"; 
+        // See if it can locate the file path
+        if (!file_exists($filePath)) {
+          echo "didnt Find the file"; 
+        } else {
+          // once the file exist, we need to read it
+          $file_handle = fopen($filePath, "r");
+
+          while (!feof($file_handle)) {
+            $line = fgets($file_handle);
+            echo "<p>". $line . "</p>" ."<br>";
+        }
+
+        // Close the file handle
+        fclose($file_handle);
+        }
+      ?>
     </section>
 
-    <script src="js/index.js"></script>
+    <script defer src="js/index.js"></script>
   </body>
 </html>
--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,23 @@
 
 // Data for the card, we can put this in a relation mysql database
 const serviceData = [
-    { title: "Basic Subscription", content: "Basic Benefits", serviceButton: "You are Basic"},
-    { title: "MidSubscription", content: "Mid Benefits", serviceButton: "You are Mid"},
-    { title: "Prenium Subscription", content: "One hawt man Benefits", serviceButton: "You are cool"}
+    { title: "Basic Subscription", content: "At a price of $50 per 3-month cycle, you will get access to coffee ranging from 3 to 5 stars in a secret. This ranges from standard office coffee to the ones you see in your advertisements.", serviceButton: "You are Basic" },
+    { title: "The Mid Subscription", content: "At a price of $70 per 3-month cycle, you will get access to coffee ranging from 4 to 5 stars in a secret. This ranges from the ones you see in your advertisements to the ones you see food critics drink.", serviceButton: "You are Mid" },
+    { title: "The Math is Mathing Subscription", content: "At a price of $100 per 3-month cycle, you will get access to the coffees that Patrick would recommend as well as other coffee critics.", serviceButton: "You are cool" }
 ];
 
 
-function createCard(title, content, serviceButton ="Optional") {
+
+const newsData = [
+    { Title: "Coffee Ex-Press-O was voted the #1 glowing start-up by the Late-Love Foundation", image: "coffee" }, 
+    { Title: "Anonymous Food Blogger Patrick has rated the service 6 out of 5 stars", image: "turtle" }, 
+    { Title: "Coffee Ex-Press-O is expanding to the Southern Bay Area", image: "mid" }
+];
+
+function createCard(title, content, serviceButton ="Click Here for more ") {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('card');
+
 
     const titleElement = document.createElement("h2");
     titleElement.innerHTML = title;
@@ -20,7 +28,6 @@ function createCard(title, content, serviceButton ="Optional") {
 
     const button = document.createElement("button");
     button.innerHTML = serviceButton;
-
 
     cardDiv.appendChild(titleElement);
     cardDiv.appendChild(contentElement);
@@ -39,3 +46,12 @@ serviceData.forEach(data => {
     const card = createCard(data.title, data.content, data.serviceButton);
     cardContainer.appendChild(card);
 });
+
+
+const newsContainer = document.getElementById("newsCardContainer");
+newsData.forEach(data =>{ 
+    
+const card = createCard(data.Title, data.image); 
+newsContainer.appendChild(card)
+
+})
