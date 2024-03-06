02ae6cf3eeb3baa94affd084525c1320084027f0
Manjesh Prasad <prasadmanjesh123@gmail.com>
2024-03-06 13:21:22 -0800

    Updated docker file

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,20 @@
-FROM php 
+FROM php:apache
 
 # Update package lists and upgrade installed packages
-RUN apt-get update && apt upgrade -y --no-install-recommends \ 
+RUN apt-get update && apt upgrade -y --no-install-recommends \
         && rm -rf /var/lib/apt/lists/*
 
 #Set the working diretory inside the container
-WORKDIR /app 
+WORKDIR /app
 
 #copy everything in the current directory to the working directory in the container
 COPY . .
 
-#Expose port 3000 for the PHP server 
+#Expose port 3000 for the PHP server
 EXPOSE 3000
 
 #Set the command to run the PHP built-in server serving index.php
-CMD ["php", "-S", "0.0.0.0:3000", "public/index.php"]
+CMD ["php", "-S", "0.0.0.0:3000", "-t", "public/"]
 
 
-#echo this in order to devug later ..... 
-
+#echo this in order to devug later .....
--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,41 @@
-alert("Hello world")
+
+// Data for the card, we can put this in a relation mysql database
+const serviceData = [
+    { title: "Basic Subscription", content: "Basic Benefits", serviceButton: "You are Basic"},
+    { title: "MidSubscription", content: "Mid Benefits", serviceButton: "You are Mid"},
+    { title: "Prenium Subscription", content: "One hawt man Benefits", serviceButton: "You are cool"}
+];
+
+
+function createCard(title, content, serviceButton ="Optional") {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+
+    const titleElement = document.createElement("h2");
+    titleElement.innerHTML = title;
+
+    const contentElement = document.createElement('p');
+    contentElement.innerHTML = content;
+    
+
+    const button = document.createElement("button");
+    button.innerHTML = serviceButton;
+
+
+    cardDiv.appendChild(titleElement);
+    cardDiv.appendChild(contentElement);
+    cardDiv.appendChild(button);
+
+    return cardDiv;
+}
+
+
+
+
+
+const cardContainer = document.getElementById("cardContainer");
+
+serviceData.forEach(data => {
+    const card = createCard(data.title, data.content, data.serviceButton);
+    cardContainer.appendChild(card);
+});
